c1bebb986ff2eeee791d4cd03cc5ed04d81047a5
mrfontana <kheartse@yahoo.com>
2022-05-16 00:24:57 -0700

    Module 4 stopping point

---
+++ home.js
@@ -0,0 +1,12 @@
+showMessage("Title...");
+
+// let total = 49.99,
+// 	price = 0,
+// 	product = 'Hiking Boots',
+// 	discounted = true;
+
+const price = 40;
+price = 29.99;
+
+
+showMessage(price);
