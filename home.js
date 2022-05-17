d21759cf984d10754fbc0297823b70991af72dbe
mrfontana <kheartse@yahoo.com>
2022-05-17 10:49:35 -0700

    Messing with ternary

--- home.js
+++ home.js
@@ -1,6 +1,15 @@
-let person = {
-	firstName: 'Kevin',
-	lastName: 'Fontana'
-};
+// Quirks when working with math in javascript
+/*
+let notRounded = 1.1 + 1.3;
+let roundedButWrong = (1.1 + 1.3).toFixed(2);
+let roundedCorrectly = +(1.1 + 1.3).toFixed(2);
 
-showMessage(person.firstName);
+console.log(notRounded); // 2.4000000000000004
+console.log(roundedButWrong); // 2.40
+console.log(roundedCorrectly); // 2.4
+*/
+
+let price = 20;
+let message = ( price > 10 ) ? 'expensive' : 'cheap';
+
+showMessage(message);
