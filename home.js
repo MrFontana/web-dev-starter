0149d71601bfec40693e0864d5302ab175ab388b
mrfontana <kheartse@yahoo.com>
2022-05-24 17:48:21 -0700

    Loop refresher some function refresher

--- home.js
+++ home.js
@@ -1,4 +1,3 @@
-// Quirks when working with math in javascript
 /*
 let notRounded = 1.1 + 1.3;
 let roundedButWrong = (1.1 + 1.3).toFixed(2);
@@ -9,7 +8,28 @@ console.log(roundedButWrong); // 2.40
 console.log(roundedCorrectly); // 2.4
 */
 
-let price = 20;
-let message = ( price > 10 ) ? 'expensive' : 'cheap';
+// let price = 20;
+// let message = ( price > 10 ) ? 'yes' : 'no';
+// showMessage(message);
 
-showMessage(message);
+// for (let i = 0; i < 5; i++) {
+// 	console.log(i);
+// };
+
+// let count = 1;
+// while (count < 5) {
+// 	console.log(count);
+// 	count++;
+// }
+
+// let count = 1;
+// do {
+// 	console.log(count);
+// 	count++;
+// } while ( count < 5);
+
+// function logMessage() {
+// 	console.log('Here is a message');
+// };
+
+// logMessage();
