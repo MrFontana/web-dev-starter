317a60c703f574404be528525cfe66c3abebb8b8
mrfontana <kheartse@yahoo.com>
2022-05-24 18:20:27 -0700

    finished module on functions

--- home.js
+++ home.js
@@ -28,8 +28,40 @@ console.log(roundedCorrectly); // 2.4
 // 	count++;
 // } while ( count < 5);
 
-// function logMessage() {
-// 	console.log('Here is a message');
+// let myFunction = function (message, firstName) {
+// 	console.log(message);
+// 	console.log(firstName);
 // };
 
-// logMessage();
+// myFunction('Hello', 'John');
+
+// function getSecretCode(value) {
+	
+// 	let code = value * 42;
+// 	return code;
+
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage( secretCode );
+
+let key = 42;
+
+// function getSecretCode(value) {
+
+// 	let keyGenerator = function() {
+// 		let key = 12;
+// 		console.log('in keyGenerator: ', key);
+// 		return key;
+// 	}
+	
+// 	let code = value * keyGenerator();
+// 	console.log(' in getSecretCode: '), key;
+// 	return code;
+
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage( secretCode );
+
+changePercentOff(32);
